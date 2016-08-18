c5070279d1d8ba4455971f8ff3d1b75a2e2a4401
juRiii <juRiii.r3@gmail.com>
2016-08-18 21:01:16 +0200

    remove old files

--- vibrance.GUI/ISettingsController.cs
+++
@@ -1,9 +0,0 @@
-namespace vibrance.GUI
-{
-    internal interface ISettingsController
-    {
-        bool setVibranceSettings(string ingameLevel, string windowsLevel, string keepActive, string refreshRate, string affectPrimaryMonitorOnly);
-        bool setVibranceSetting(string szKeyName, string value);
-        void readVibranceSettings(GraphicsAdapter graphicsAdapter, out int vibranceIngameLevel, out int vibranceWindowsLevel, out bool keepActive, out int refreshRate, out bool affectPrimaryMonitorOnly);
-    }
-}

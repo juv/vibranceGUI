614a98e89467d46752a0ae7e44ab795406bff037
juvlarN <arr9gan7@gmail.com>
2016-08-22 00:20:05 +0200

    Added Process Explorer

---
+++ vibrance.GUI/common/ProcessExplorer.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace vibrance.GUI.common
+{
+    partial class ProcessExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessExplorer));
+            this.listView = new System.Windows.Forms.ListView();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
+            this.button = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(12, 41);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(615, 236);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // iconList
+            // 
+            this.iconList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.iconList.ImageSize = new System.Drawing.Size(16, 16);
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(13, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(119, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Reload Processes";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // ProcessExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 289);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.listView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ProcessExplorer";
+            this.Text = "vibranceGUI Process Explorer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList iconList;
+        private System.Windows.Forms.Button button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
---
+++ vibrance.GUI/common/ProcessExplorer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vibrance.GUI.common
+{
+    public partial class ProcessExplorer : Form
+    {
+        public ProcessExplorer()
+        {
+            InitializeComponent();
+
+            listView.Columns.Add("Programs", 130, HorizontalAlignment.Left);
+            listView.Columns.Add("Full Path", 320, HorizontalAlignment.Left);
+            listView.LargeImageList = iconList;
+            listView.FullRowSelect = true;
+            listView.View = View.Tile;
+            listView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void GetAllProcesses()
+        {
+            var activeProcceses = Process.GetProcesses();
+            foreach(Process process in activeProcceses)
+            {
+                if(process.MainWindowHandle != IntPtr.Zero)
+                {
+                    Console.WriteLine(String.Format("ProcessName: {0}\tMainWindowHandle {1}: ", process.ProcessName, process.MainWindowHandle));
+                    string path = string.Empty; 
+                    try
+                    {
+                        path = GetPathFromProcessId(process);
+                        if (path != string.Empty)
+                        {
+                            iconList.Images.Add(Icon.ExtractAssociatedIcon(path));
+                            var listItem = new ListViewItem(process.ProcessName, iconList.Images.Count - 1);
+                            listItem.Tag = process;
+                            listItem.SubItems.Add(path);
+                            listView.Items.Add(listItem);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+
+        private string GetPathFromProcessId(Process process)
+        {
+            var query = "SELECT ProcessId, ExecutablePath, CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id;
+
+            using (var searcher = new ManagementObjectSearcher(query))
+            foreach (ManagementObject item in searcher.Get())
+            {
+                object id = item["ProcessID"];
+                object path = item["ExecutablePath"];
+
+                if (path != null && id.ToString() == process.Id.ToString())
+                {
+                    return path.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            if(listView.SelectedItems.Count == 1)
+            {
+                Console.WriteLine(listView.SelectedItems[0].Text);
+                Console.WriteLine(listView.SelectedItems[0].Tag);
+
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            listView.Items.Clear();
+            GetAllProcesses();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    GetAllProcesses();
+                });
+            }
+            else
+            {
+                GetAllProcesses();
+            }
+        }
+    }
+}

1347ba7d79f6ffd39d835dd09e8935c0350d9fce
juvlarN <arr9gan7@gmail.com>
2016-08-22 18:27:10 +0200

    Moved ProcessExplorerEntry to its own file

---
+++ vibrance.GUI/common/ProcessExplorerEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace vibrance.GUI.common
+{
+
+    public class ProcessExplorerEntry
+    {
+        public string Path { get; set; }
+
+        public Icon Icon { get; set; }
+
+        public Process Process { get; set; }
+
+        public ProcessExplorerEntry(string path, Icon icon, Process process)
+        {
+            this.Path = path;
+            this.Icon = icon;
+            this.Process = process;
+        }
+    }
+}

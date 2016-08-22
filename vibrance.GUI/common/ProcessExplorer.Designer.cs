86526f69bddb87409a65f721e47570025b083497
juvlarN <arr9gan7@gmail.com>
2016-08-22 19:06:35 +0200

    Changed ProcessExplorers Form to start centered

--- vibrance.GUI/common/ProcessExplorer.Designer.cs
+++ vibrance.GUI/common/ProcessExplorer.Designer.cs
@@ -76,6 +76,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ProcessExplorer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "vibranceGUI Process Explorer";
             this.ResumeLayout(false);
 

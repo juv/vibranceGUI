25f69a103f8882023488c31415c1f0d116ded48c
juvlarN <arr9gan7@gmail.com>
2017-10-24 22:27:11 +0200

    Pushed assembly version to 2.3.0.6

--- vibrance.GUI/Properties/AssemblyInfo.cs
+++ vibrance.GUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.5")]
-[assembly: AssemblyFileVersion("2.3.0.5")]
+[assembly: AssemblyVersion("2.3.0.6")]
+[assembly: AssemblyFileVersion("2.3.0.6")]

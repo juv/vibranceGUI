bb70be40593b510787c39818f7541a9e692ad51c
juvlarN <arr9gan7@gmail.com>
2017-11-08 15:08:51 +0100

    Set MaximizeBox property to false for program settings form

--- vibrance.GUI/common/VibranceSettings.Designer.cs
+++ vibrance.GUI/common/VibranceSettings.Designer.cs
@@ -156,6 +156,7 @@
             this.Controls.Add(this.groupBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "VibranceSettings";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "vibranceGUI";
